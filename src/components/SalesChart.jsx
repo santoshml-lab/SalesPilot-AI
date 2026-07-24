737431baa7cbaeb6c0c9c3a700d5270c65412fd0
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 00:34:56 +0530

    Update SalesChart.jsx

--- src/components/SalesChart.jsx
+++ src/components/SalesChart.jsx
@@ -1,1 +1,75 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid
+} from "recharts";
 
+
+const data = [
+  {
+    month: "Jan",
+    sales: 50000
+  },
+  {
+    month: "Feb",
+    sales: 80000
+  },
+  {
+    month: "Mar",
+    sales: 120000
+  },
+  {
+    month: "Apr",
+    sales: 150000
+  },
+  {
+    month: "May",
+    sales: 200000
+  }
+];
+
+
+export default function SalesChart(){
+
+  return (
+
+    <div>
+
+      <h2>
+        Sales Growth
+      </h2>
+
+
+      <ResponsiveContainer width="100%" height={300}>
+
+        <LineChart data={data}>
+
+          <CartesianGrid strokeDasharray="3 3" />
+
+          <XAxis dataKey="month" />
+
+          <YAxis />
+
+          <Tooltip />
+
+
+          <Line
+            type="monotone"
+            dataKey="sales"
+            strokeWidth={3}
+          />
+
+        </LineChart>
+
+      </ResponsiveContainer>
+
+
+    </div>
+
+  );
+
+}
