227a17ffdde6f931ff334a3e2515b2ec640b75eb
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 00:10:40 +0530

    Update StatCard.jsx

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,1 +1,20 @@
 
+export default function StatCard({ title, value, icon }) {
+
+  return (
+
+    <div>
+
+      <h3>
+        {icon} {title}
+      </h3>
+
+      <h2>
+        {value}
+      </h2>
+
+    </div>
+
+  );
+
+}
