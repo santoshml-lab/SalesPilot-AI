c9d90ddd6710f398799e95c5e1c6374ecda8b9c8
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 00:13:01 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import StatCard from "../components/StatCard";
 
 export default function Dashboard() {
 
@@ -12,6 +13,39 @@ export default function Dashboard() {
       <p>
         Welcome to SalesPilot AI CRM
       </p>
+
+
+      <div>
+
+        <StatCard
+          title="Revenue"
+          value="₹2,45,000"
+          icon="💰"
+        />
+
+
+        <StatCard
+          title="Customers"
+          value="1,284"
+          icon="👥"
+        />
+
+
+        <StatCard
+          title="Deals"
+          value="68"
+          icon="💼"
+        />
+
+
+        <StatCard
+          title="Conversion"
+          value="31%"
+          icon="📈"
+        />
+
+      </div>
+
 
     </div>
 
