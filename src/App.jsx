57b898b375bf505d47374b4628e8cb117a0bbcea
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 01:33:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 
-
-export default function App(){
+export default function App() {
 
   return (
+
     <div>
-      <h1>
-        SalesPilot AI Running 🚀
-      </h1>
+
+      <Sidebar />
+
+      <Navbar />
+
+      <Dashboard />
+
     </div>
+
   );
 
 }
