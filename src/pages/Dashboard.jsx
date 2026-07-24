834a6fbf00b6bfe1986d7781250607357ba0ac68
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 00:05:58 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,20 @@
 
+export default function Dashboard() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Sales Dashboard
+      </h1>
+
+      <p>
+        Welcome to SalesPilot AI CRM
+      </p>
+
+    </div>
+
+  );
+
+}
