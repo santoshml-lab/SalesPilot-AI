09200f78e86c8a43acfef0b4dcc223bd377115f7
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-24 23:59:39 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav>
 
+      <h3>
+        SalesPilot AI
+      </h3>
+
+      <div>
+        🔔 Notifications
+        <br />
+        👤 Admin
+      </div>
+
+    </nav>
+  );
+}
