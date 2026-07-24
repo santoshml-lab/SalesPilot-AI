b7a4ee41e8df90fdfe6ac29f7e85c6217fc3476c
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-24 23:56:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Sidebar from "./components/Sidebar";
+
 export default function App() {
+
   return (
+
     <div>
-      <h1>SalesPilot AI</h1>
-      <p>Welcome to AI CRM SaaS</p>
+
+      <Sidebar />
+
     </div>
+
   );
+
 }
