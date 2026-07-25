fe7b3248e78574384139445b83716ad6b64d09bb
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 11:42:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
+
+  const [page, setPage] = useState("login");
+
+  if (page === "login") {
+    return <Login setPage={setPage} />;
+  }
+
+  if (page === "signup") {
+    return <Signup setPage={setPage} />;
+  }
+
   return (
     <div style={{ display: "flex" }}>
       <Sidebar />
@@ -11,6 +27,7 @@ export default function App() {
         <Navbar />
         <Dashboard />
       </div>
+
     </div>
   );
 }
