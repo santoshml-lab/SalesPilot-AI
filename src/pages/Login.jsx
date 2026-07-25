f21656bd0f7391b22c3352d9dcf3d63cafae6151
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-25 11:52:32 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,49 +3,29 @@ import "../styles/login.css";
 export default function Login({ setPage }) {
   return (
     <div className="login-container">
-
       <div className="login-card">
-
         <h1>SalesPilot AI</h1>
 
         <p>Login to your account</p>
 
-        <input
-          type="email"
-          placeholder="Email"
-        />
+        <input type="email" placeholder="Email" />
 
-        <input
-          type="password"
-          placeholder="Password"
-        />
+        <input type="password" placeholder="Password" />
 
         <button onClick={() => setPage("dashboard")}>
           Login
         </button>
 
-        <p
-          style={{
-            marginTop: "20px",
-            textAlign: "center",
-            color: "#94a3b8"
-          }}
-        >
+        <p style={{ marginTop: "20px", textAlign: "center" }}>
           Don't have an account?{" "}
           <span
+            style={{ color: "#3b82f6", cursor: "pointer" }}
             onClick={() => setPage("signup")}
-            style={{
-              color: "#3b82f6",
-              cursor: "pointer",
-              fontWeight: "bold"
-            }}
           >
             Sign Up
           </span>
         </p>
-
       </div>
-
     </div>
   );
 }
